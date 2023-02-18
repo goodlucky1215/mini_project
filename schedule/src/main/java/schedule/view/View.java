cd06aad4269ee7becbbb7719be88c04779e5f1f5
jiny <goodlucky1215@naver.com>
2023-02-18 21:50:16 +0900

    화면 그리는 중

--- schedule/src/main/java/schedule/view/View.java
+++ schedule/src/main/java/schedule/view/View.java
@@ -1,10 +1,17 @@
 package schedule.view;
 
 import java.awt.Button;
+import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.GridLayout;
 import java.awt.Panel;
 
-public class View extends Frame{
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class View extends JFrame{
 	
 	//	String startScheduleDay = "20230219"; //시작날짜 입력받음
 	//int[] morningPeople =   {3,2,3,2,3,3,3}; // 월-일 입력 받음
@@ -16,8 +23,19 @@ public class View extends Frame{
 	// 버튼 생성자
 	public View(String str){
 		super(str);
-		// 버튼을 생성하기위해 패널 생성
+		//날짜 입력
+		JPanel dayPanel =new JPanel();
+		dayPanel.setLayout(new GridLayout(1,3)); 
+		JTextField day = new JTextField(10); 
+		dayPanel.add(new JLabel("시작 날짜를 입력하세요.", JLabel.LEFT));
+		dayPanel.add(day);
+		dayPanel.add(new JLabel("ex) 20230101"));
+		dayPanel.setToolTipText("ID를 입력하세요");
+		dayPanel.setSize(new Dimension(200,100));
+/*
+		
 		Panel p = new Panel();
+		Panel p2 = new Panel();
 		// 가위 버튼 생성
 		btn1 = new Button(" 가위 ");
 		// 바위 버튼 생성
@@ -25,12 +43,15 @@ public class View extends Frame{
 		// 보 버튼 생성
 		btn3 = new Button("  보  ");
 		// 패널에 3가지 버튼 생성 -> 이거 해야 화면에 보임
-		p.add(btn1); p.add(btn2); p.add(btn3);
-		add(p);
+		p.add(btn1); p.add(btn2); p2.add(btn3);
 		btn3.setEnabled(false); //버튼 비활성화 여부 지정
-		setSize(800, 800);
+		*/
+		
+		getContentPane().add(dayPanel,"Center");
 		// 윈도우창 띄우기
-		setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(800, 800);
+		this.setVisible(true);
 	}
 	
 	public static void main(String args[]){
