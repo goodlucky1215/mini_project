aba388a4e641a5f18335a8a4cb550ae9a7de5d13
jiny <goodlucky1215@naver.com>
2023-02-20 17:20:56 +0900

    보여지는 화면 완성

--- schedule/src/main/java/schedule/view/View.java
+++ schedule/src/main/java/schedule/view/View.java
@@ -6,10 +6,15 @@ import java.awt.Frame;
 import java.awt.GridLayout;
 import java.awt.Panel;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 public class View extends JFrame{
 	
@@ -18,56 +23,41 @@ public class View extends JFrame{
 	//int[] afternoonPeople = {2,2,2,2,2,3,3}; // 월-일 입력 받음
 	//int[] dayOffPeople =    {2,3,2,3,2,1,1}; // 월-일 쉬는 사람
 	
-	// 버튼 변수 선언
-	Button btn1, btn2, btn3;
-	// 버튼 생성자
+	String cols[] = {"","월","화","수","목","금","토","일"}     ; // 컬럼에 헤더 값
+	JTextField day;
+	JButton button;
+	// 화면
 	public View(String str){
 		super(str);
 		//날짜 입력
-		JPanel dayPanel =new JPanel();
-		dayPanel.setLayout(new GridLayout(1,3)); 
-		JTextField day = new JTextField(10); 
+		JPanel dayPanel =new JPanel(); 
+		this.add(dayPanel,"Center");
+				
+		day = new JTextField(10);
+		day.setBounds(12, 340, 560, 25); // 채팅 입력창 위치지정
 		dayPanel.add(new JLabel("시작 날짜를 입력하세요.", JLabel.LEFT));
 		dayPanel.add(day);
 		dayPanel.add(new JLabel("ex) 20230101"));
-		dayPanel.setToolTipText("ID를 입력하세요");
-		dayPanel.setSize(new Dimension(200,100));
-		
-		JPanel weekPanel =new JPanel();
-		weekPanel.setLayout(new GridLayout(3,7)); 
-		weekPanel.add(new JLabel("월", JLabel.LEFT));
-		weekPanel.add(new JLabel("월", JLabel.LEFT));
-		weekPanel.add(new JLabel("월", JLabel.LEFT));
-		weekPanel.add(new JLabel("월", JLabel.LEFT));
-		weekPanel.add(new JLabel("월", JLabel.LEFT));
-		weekPanel.add(new JLabel("월", JLabel.LEFT));
-		weekPanel.add(new JLabel("월", JLabel.LEFT));
-		weekPanel.add(new JLabel("월", JLabel.LEFT));
+		dayPanel.add(new JLabel("7명 기준 : 근무 총 합 35, 쉬는 수 총 합 14"));
+		//시간표 테이블
+	    String data[][] = new String[3][8];
+	    data[0][0] = "오전 사람 수";
+	    data[1][0] = "오후 사람 수";
+	    data[2][0] = "쉬는 사람 수";
+		JTable jtm = new JTable();
+	    jtm.setModel(new DefaultTableModel(data,cols));
+	    TableColumn col = jtm.getColumnModel().getColumn(0);
+	    col.setPreferredWidth(120);
+	    JScrollPane spTable = new JScrollPane(jtm);
+	    dayPanel.add(spTable);
+	    
+	    //엑셀 만드는 버튼
+	    dayPanel.add(button = new JButton("전송")); // 전송버튼 생성 및 부착
 
-		
-		
-		
 
-/*
-		
-		Panel p = new Panel();
-		Panel p2 = new Panel();
-		// 가위 버튼 생성
-		btn1 = new Button(" 가위 ");
-		// 바위 버튼 생성
-		btn2 = new Button(" 바위 ");
-		// 보 버튼 생성
-		btn3 = new Button("  보  ");
-		// 패널에 3가지 버튼 생성 -> 이거 해야 화면에 보임
-		p.add(btn1); p.add(btn2); p2.add(btn3);
-		btn3.setEnabled(false); //버튼 비활성화 여부 지정
-		*/
-		
-		getContentPane().add(dayPanel,"Center");
-		getContentPane().add(weekPanel,"South");
 		// 윈도우창 띄우기
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setSize(800, 800);
+		this.setSize(500, 600);
 		this.setVisible(true);
 	}
 	
@@ -75,4 +65,5 @@ public class View extends JFrame{
 		// 버튼 생성자
 		new View("스케줄 화면");
 	}
+	
 }
